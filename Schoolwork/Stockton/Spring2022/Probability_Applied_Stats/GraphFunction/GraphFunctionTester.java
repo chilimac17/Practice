da38d1f04fbd0dbde18807a730553eb5e4286fe6
Michael Chillemi <michaelchillemi17@gmail.com>
2023-07-22 14:02:03 -0400

    adding in graph function

---
+++ Schoolwork/Stockton/Spring2022/Probability_Applied_Stats/GraphFunction/GraphFunctionTester.java
@@ -0,0 +1,15 @@
+package GraphFunction;
+
+
+public class GraphFunctionTester {
+    public static void main(String[] args){
+        /**
+         * Creating object, running results method
+         */
+        Results run = new Results();
+        run.results();
+       
+       
+    }
+}
+
