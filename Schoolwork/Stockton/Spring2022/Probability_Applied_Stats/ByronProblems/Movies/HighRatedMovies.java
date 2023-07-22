6ec77c2b37218563bbd0cb047037d0cd571b692b
Michael Chillemi <michaelchillemi17@gmail.com>
2023-07-22 13:51:30 -0400

    adding personal byron problems

---
+++ Schoolwork/Stockton/Spring2022/Probability_Applied_Stats/ByronProblems/Movies/HighRatedMovies.java
@@ -0,0 +1,20 @@
+package ByronProblems.Movies;
+
+//import java.util.Map;
+
+public class HighRatedMovies {
+    /*
+    public Map<Movie> getHighRatedMovies(Movie someMovie, int n){
+        Map<Movie> highRatedMovies = new Map<>();
+        if(n == 0){
+            return highRatedMovies;
+        }
+        if(n > someMovie.getRelatedMovies().size()){
+            highRatedMovies.putAll(someMovie.getRelatedMovies());
+        }
+
+
+        return highRatedMovies;
+    }
+    */
+}
---
+++ Schoolwork/Stockton/Spring2022/Probability_Applied_Stats/ByronProblems/Movies/Movie.java
@@ -0,0 +1,23 @@
+package ByronProblems.Movies;
+
+//import java.util.Map;
+
+public class Movie {
+    //private int id;
+    //private double rating;
+    /*
+    Map<String,Movie> relatedMovies = new Map<>();
+
+
+    public int getId(){
+        return id;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public Map<String,Movie> getRelatedMovies(){
+        return relatedMovies;
+    }
+    */
+
+}
